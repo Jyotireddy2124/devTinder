49a39992138f32025048d2de1039861cd8603a9d
nagjyoti_tokaly <nagjyoti_tokaly@epam.com>
2026-05-10 16:04:54 +0530

    exploring GET,POST DELETE methods and routing

--- src/app.js
+++ src/app.js
@@ -5,13 +5,33 @@ const app = express();
 
 //using server
 
-
-app.use("/",(req,res) => {
+// This will override all the below methods because it is starting with "/"
+/* app.use("/",(req,res) => {
     res.send("Home page")
-})
+}) */
 
 app.use("/test", (req,res) => {
     res.send("Hello from test")
+})
+
+// This will override all the below /user methods because it is starting with "/"
+app.use("/user" , (req,res) => {
+    //save data to DB
+    res.send("user details");
+})
+
+//This will only handle GET call to /user
+app.get("/user", (req,res) => {
+    res.send({firstname : "Jyoti" , lastname : "Reddy"});
+});
+
+app.post("/user", (req,res) => {
+    //save data to DB
+    res.send("Data Successfully saved to the database");
+});
+
+app.delete("/user" , (req,res) => {
+    res.send("User Deleted successfully")
 })
 
 //listening on port 3000
